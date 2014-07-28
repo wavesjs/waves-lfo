a54dd7cc69051fbe0e6c7d47539bbf974fe1f161
Victor Saiz <vsaiz@ircam.fr>
2014-07-28 13:02:46 +0200

    api breaking changes; more to come

--- index.js
+++ index.js
@@ -1,26 +1,40 @@
 "use strict";
 
+var pck = require('./package.json');
 var LFP = require('../lfp');
 var _lfp = Object.create(LFP); // inherit from base lfp
 
-var newFrame = new Float32Array(1);
+Object.defineProperties(_lfp, {
+  type: {value: pck.name},
+  newFrame: {writable: true},
+  frameSize : {writable: true} // freameoutout
+});
 
-Object.defineProperty(_lfp, 'processScalar', {
-  value: function(data) {
+Object.defineProperty(_lfp, 'init', {
+  value: function() {
+    this.newFrame = new Float32Array(1);
+    this.frameSize = 1;
+    return this;
+  }
+});
 
-    var time = data[0];
-    var frame = data[1];
+Object.defineProperty(_lfp, 'process', {
+  value: function(time, frame) {
+
     var frameSize = frame.length;
     var sum = 0;
     var i;
 
-    for (i = 0; i < frameSize; i++) sum += frame[i] * frame[i];
-    newFrame.set([Math.sqrt(sum / frameSize)], 0);
-    this.nextOperator([time, newFrame]);
+    for (i = 0; i < frameSize; i++)
+      sum += (frame[i] * frame[i]);
+    
+    time -= this.offset;
+    this.newFrame.set([Math.sqrt(sum / frameSize)], 0);
+    this.nextOperator(time, this.newFrame);
   }
 });
 
 module.exports = function(opts) {
-  _lfp.type = 'rms';
-  return Object.create(_lfp.init(opts));
+  // creates an instance and returns it initialized
+  return LFP.create(_lfp, opts);
 };
