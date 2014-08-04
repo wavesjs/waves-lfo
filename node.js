f08e0525c70f1e63041c02911dd6fe6f6285b8c2
vectorsize <vectorsize@gmail.com>
2014-08-04 19:48:18 +0200

    up goes the code

---
+++ node.js
@@ -0,0 +1,27 @@
+"use strict";
+
+class Node {
+
+  constructor(graph, lfp){
+    this.graph = graph;
+    this.lfp = lfp;
+    this.nodes = [];
+  }
+
+  pipe(ctor, options = {}, id = null) {
+    var lfp = ctor(options);
+    var nextNode = new Node(this.graph, lfp);
+    
+    this.lfp.add(lfp); // add to operators
+    this.graph.add(this, nextNode, id); // add to graph: parent, child, id
+    
+    return nextNode;
+  }
+
+  remove(){
+    this.graph.remove(this);
+    this.lfp.remove(this);
+  }
+}
+
+module.exports = Node;
