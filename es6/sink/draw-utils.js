2ee4d80e0181de0c4fd84aee33741e30cb180fac
b-ma <benjamin.matuszewski@gmail.com>
2015-03-16 15:04:41 +0100

    moved `getRandomColor` to `draw-utils`

---
+++ es6/sink/draw-utils.js
@@ -0,0 +1,9 @@
+
+module.exports.getRandomColor = function() {
+  var letters = '0123456789ABCDEF'.split('');
+  var color = '#';
+  for (var i = 0; i < 6; i++ ) {
+      color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
