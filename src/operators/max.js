77df8ca249572fb28f44666e076740f6c476f902
Benjamin Matuszewski <benjamin.matuszewski@gmail.com>
2016-08-19 18:08:16 +0200

    tested new API

--- src/operators/max.js
+++ src/operators/max.js
@@ -4,26 +4,77 @@ import BaseLfo from '../core/base-lfo';
 /**
  * Output the max value of the current frame.
  * @todo - define if their are options
+ * @note - not sure if it's usefull, but let's use it as a exercice
  */
 export default class Max extends BaseLfo {
   constructor(options) {
-    super({}, options);
+    super({
+      order: 1,
+    }, options);
+
+    // 'dyanmic' because should call it's own initialize method, even if
+    // there is no need to propagate to chidren
+    this.addIntegerParam('order', 1, Infinity, 'dynamic');
   }
 
   initialize(inStreamParams) {
-    super.initialize(inStreamParams, {
-      frameSize: 1,
-    });
+    super.initialize(inStreamParams);
+
+    const bufferSize = this.getParam('order') * this.streamParams.frameSize;
+    this.ringBuffer = new Float32Array(bufferSize);
+    this.ringIndex = 0;
+  }
+
+  inputScalar(value) {
+    const order = this.getParam('order');
+    const ringBuffer = this.ringBuffer;
+    const ringIndex = this.ringIndex;
+    let max = -Infinity;
+
+    this.ringBuffer[ringIndex] = value;
+
+    for (let i = 0; i < order; i++) {
+      if (ringBuffer[i] > max)
+        max = ringBuffer[i]
+    }
+
+    this.ringIndex = (ringIndex + 1) % order;
+
+    return max;
+  }
+
+  inputArray(frame) {
+    const order = this.getParam('order');
+    const frameSize = this.streamParam.frameSize;
+    const outFrame = this.outFrame;
+    const ringBuffer = this.ringBuffer;
+    const ringIndex = this.ringIndex;
+    const ringOffset = ringIndex * order;
+
+    for (let i = 0; i < frameSize; i++) {
+      outFrame[i] = -Infinity;
+      ringBuffer[ringOffset + i] = frame[i];
+
+      for (let j = 0; j < order; j++) {
+        const index = j * order + i;
+
+        if (ringBuffer[index] > outFrame[i])
+          outFrame[i] = ringBuffer[index];
+      }
+    }
+
+    this.ringIndex = (ringIndex + 1) % order;
+
+    return outFrame;
   }
 
   process(time, frame, metaData) {
-    let max = -Infinity;
-
-    for (let i = 0; i < frame.length; i++)
-      if (frame[i] > max) max = frame[i];
+    if (this.streamParam.frameSize > 1)
+      this.inputArray(frame)
+    else
+      this.outFrame[0] = this.inputScalar(frame[0]);
 
     this.time = time;
-    this.outFrame[0] = max;
     this.metaData = metaData;
 
     this.output();
