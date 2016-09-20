4d7270748b6bbd6b2d46ccd7f6c68379fbc17afe
Benjamin Matuszewski <benjamin.matuszewski@gmail.com>
2016-09-20 15:35:55 +0200

    updated Magnitude operator

--- src/operator/Magnitude.js
+++ src/operator/Magnitude.js
@@ -24,6 +24,31 @@ const definitions = {
  *  the vector size.
  * @param {Boolean} [options.power=false] - If true, returns the squared
  *  magnitude (power).
+ *
+ * @todo - Define if output should be a `vector` or a `scalar`
+ *
+ * @memberof module:operator
+ * @example
+ * import * as lfo from 'waves-lfo';
+ *
+ * const eventIn = new lfo.source.EventIn({ frameSize: 2, frameType: 'vector' });
+ * const magnitude = new lfo.operator.Magnitude();
+ * const logger = new lfo.sink.Logger({ outFrame: true });
+ *
+ * eventIn.connect(magnitude);
+ * magnitude.connect(logger);
+ * eventIn.start();
+ *
+ * eventIn.processFrame(null, [1, 1]);
+ * > [1]
+ * eventIn.processFrame(null, [2, 2]);
+ * > [2]
+ * eventIn.processFrame(null, [3, 3]);
+ * > [3]
+ * eventIn.processFrame(null, [4, 4]);
+ * > [2, 2]
+ * eventIn.processFrame(null, [5, 5]);
+ * > [3, 3]
  */
 class Magnitude extends BaseLfo {
   constructor(options) {
@@ -36,6 +61,7 @@ class Magnitude extends BaseLfo {
     this._power = this.params.get('power');
   }
 
+  /** @private */
   onParamUpdate(name, value, metas) {
     super.onParamUpdate(name, value, metas);
 
@@ -49,28 +75,34 @@ class Magnitude extends BaseLfo {
     }
   }
 
-  processStreamParams(prevStreamParams) {
+  /** @private */
+  processStreamParams(prevStreamParams = {}) {
     this.prepareStreamParams(prevStreamParams);
     this.streamParams.frameSize = 1;
+    this.streamParams.frameType = 'scalar';
+    this.streamParams.description = ['magnitude'];
     this.propagateStreamParams();
   }
 
+  /** @private */
   processVector(frame) {
-    const mag = this.inputVector(frame.data);
-    this.frame.data[0] = mag;
+    this.frame.data[0] = this.inputVector(frame.data);
   }
 
+  /**
+   * @todo
+   */
   inputVector(vector) {
-    const vectorLength = vector.length;
+    const length = vector.length;
     let sum = 0;
 
-    for (let i = 0; i < vectorLength; i++)
+    for (let i = 0; i < length; i++)
       sum += (vector[i] * vector[i]);
 
     let mag = sum;
 
     if (this._normalize)
-      mag /= vectorLength;
+      mag /= length;
 
     if (!this._power)
       mag = sqrt(mag);
