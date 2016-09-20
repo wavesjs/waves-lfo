50ba14285c4fcd667c911537cc6821097982caa6
Benjamin Matuszewski <benjamin.matuszewski@gmail.com>
2016-09-20 15:37:50 +0200

    updated MinMax operator

---
+++ src/operator/MinMax.js
@@ -0,0 +1,84 @@
+import BaseLfo from '../core/BaseLfo';
+
+/**
+ * Find the minimun and maximum values of a given signal at each frame.
+ *
+ * @memberof module:operator
+ *
+ * @example
+ *  const eventIn = new EventIn({
+ *   frameSize: 512,
+ *   frameType: 'signal',
+ *   sampleRate: 0,
+ * });
+ *
+ * const minMax = new MinMax();
+ *
+ * eventIn.connect(minMax);
+ * eventIn.start()
+ *
+ * // create a signal frame
+ * const signal = new Float32Array(512);
+ * for (let i = 0; i < 512; i++)
+ *   signal[i] = i + 1;
+ *
+ * eventIn.process(null, signal);
+ * > [1, 512];
+ */
+class MinMax extends BaseLfo {
+  /** private */
+  processStreamParams(prevStreamParams= {}) {
+    this.prepareStreamParams(prevStreamParams);
+
+    this.streamParams.frameType = 'vector';
+    this.streamParams.frameSize = 2;
+    this.streamParams.description = ['min', 'max'];
+
+    this.propagateStreamParams();
+  }
+
+  /**
+   * Allows for the use of a `minMax` outside a graph (e.g. inside
+   * another node), in this case `processStreamParams` and `resetStream`
+   * should be called manually on the node.
+   *
+   * @param {Float32Array|Array} data - Signal to feed the operator with.
+   * @return {Array} - Array containing the min and max values.
+   *
+   * @example
+   * const minMax = new MinMax();
+   * // the input frame size must be defined manually as it is not
+   * // forwarded by a parent node
+   * minMax.processStreamParams({ frameSize: 10 });
+   * minMax.resetStream();
+   *
+   * minMax.inputSignal([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+   * > [0, 5]
+   *
+   * @see {@link module:core.BaseLfo#processStreamParams}
+   * @see {@link module:core.BaseLfo#resetStream}
+   */
+  inputSignal(data) {
+    const outData = this.frame.data;
+    let min = +Infinity;
+    let max = -Infinity;
+
+    for (let i = 0, l = data.length; i < l; i++) {
+      const value = data[i];
+      if (value < min) min = value;
+      if (value > max) max = value;
+    }
+
+    outData[0] = min;
+    outData[1] = max;
+
+    return outData;
+  }
+
+  /** private */
+  processSignal(frame) {
+    this.inputSignal(frame.data);
+  }
+}
+
+export default MinMax;
---
+++ tests/operator.MinMax.test.js
@@ -0,0 +1,65 @@
+import Asserter from './Asserter';
+import MinMax from '../src/operator/MinMax';
+import EventIn from '../src/source/EventIn';
+import tape from 'tape';
+
+
+tape('MinMax', (t) => {
+
+  t.comment('processSignal');
+
+  const eventIn = new EventIn({
+    frameSize: 512,
+    frameType: 'signal',
+    sampleRate: 0,
+  });
+
+  const minMax = new MinMax();
+  const asserter = new Asserter(t);
+
+  eventIn.connect(minMax);
+  minMax.connect(asserter);
+
+  // prepare test frame and expected frame
+  const signal = new Float32Array(512);
+
+  for (let i = 0; i < 512; i++)
+    signal[i] = Math.random();
+
+  const frame = {
+    time: Math.random(),
+    data: signal,
+    metadata: { a: 'a' },
+  };
+
+  const expected = {
+    time: frame.time,
+    data: [Math.min.apply(null, signal), Math.max.apply(null, signal)],
+    metadata: frame.metadata,
+  };
+
+  asserter.setExpectedFrame(expected);
+
+  eventIn.start();
+  eventIn.processFrame(frame);
+
+  t.comment('inputSignal');
+
+  const input = new Float32Array(512);
+
+  for (let i = 0; i < 512; i++)
+    input[i] = Math.random();
+
+  const output = minMax.inputSignal(input);
+
+  t.deepEqual(output[0], Math.min.apply(null, input), 'should have proper min');
+  t.deepEqual(output[1], Math.max.apply(null, input), 'should have proper max');
+
+  t.end();
+});
+
+
+
+
+
+
