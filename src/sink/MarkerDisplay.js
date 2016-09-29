b4258994816a584fbbed9557196f6bf0c0e36fd7
Benjamin Matuszewski <benjamin.matuszewski@gmail.com>
2016-09-29 19:06:20 +0200

    updated MarkerDisplay

---
+++ src/sink/MarkerDisplay.js
@@ -0,0 +1,68 @@
+import BaseDisplay from './BaseDisplay';
+import { getRandomColor } from '../utils/display-utils';
+
+const definitions = {
+  threshold: {
+    type: 'float',
+    default: 0,
+    metas: { kind: 'dynamic' },
+  },
+  thresholdIndex: {
+    type: 'integer',
+    default: null,
+    nullable: true,
+    metas: { kind: 'dynamic' },
+  },
+  color: {
+    type: 'string',
+    default: null,
+    nullable: true,
+    metas: { kind: 'dynamic' },
+  }
+};
+
+/**
+ * Display a marker according to the input frame.
+ *
+ * @param {String} color - Color of the marker.
+ * @param {Number} thresholdIndex - Index of the incomming frame data to compare
+ *  against the threshold. _Should be used in conjonction with `threshold`_.
+ * @param {Number} threshold - Minimum value the incomming value should have to
+ *  trigger the display of a marker. _Should be used in conjonction with
+ *  `thresholdIndex`_.
+ */
+class MarkerDisplay extends BaseDisplay {
+  constructor(options) {
+    super(definitions, options);
+  }
+
+  processVector(frame, frameWidth, pixelsSinceLastFrame) {
+    const color = this.params.get('color');
+    const threshold = this.params.get('threshold');
+    const thresholdIndex = this.params.get('thresholdIndex');
+    const ctx = this.ctx;
+    const height = ctx.height;
+    let value = null;
+
+    if (thresholdIndex !== null)
+      value = frame[0];
+
+    if (value === null || value > threshold) {
+      let yMin = this.getYPosition(this.params.get('min'));
+      let yMax = this.getYPosition(this.params.get('max'));
+
+      if (yMin > yMax) {
+        const v = yMax;
+        yMax = yMin;
+        yMin = v;
+      }
+
+      ctx.save();
+      ctx.fillStyle = color;
+      ctx.fillRect(0, yMin, 1, yMax);
+      ctx.restore();
+    }
+  }
+}
+
+export default MarkerDisplay;
