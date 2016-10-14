86db88a7e9b8e6f7637d28e9b83d119db9512574
Benjamin Matuszewski <benjamin.matuszewski@gmail.com>
2016-10-14 12:09:34 +0200

    exposed BasedDisplay

--- src/sink/_namespace.js
+++ src/sink/_namespace.js
@@ -2,6 +2,8 @@
 // import Bridge from './bridge';
 // import DataRecorder from './data-recorder';
 // import Trace from './trace';
+
+import BaseDisplay from './BaseDisplay';
 import BpfDisplay from './BpfDisplay';
 import Bridge from './Bridge';
 import Logger from './Logger';
@@ -13,6 +15,7 @@ import WaveformDisplay from './WaveformDisplay';
 
 
 export default {
+  BaseDisplay,
   BpfDisplay,
   Bridge,
   Logger,
