453b3c121ee132d9e14e6a5f3480b2a3a281140f
Benjamin Matuszewski <benjamin.matuszewski@gmail.com>
2016-12-07 16:22:01 +0100

    updated example to new basic-controllers

--- examples/sink-bpf-display/src/index.js
+++ examples/sink-bpf-display/src/index.js
@@ -1,5 +1,5 @@
 import * as lfo from 'waves-lfo/client';
-import * as controllers from 'waves-basic-controllers';
+import * as controllers from 'basic-controllers';
 
 const eventIn = new lfo.source.EventIn({
   frameSize: 2,
@@ -27,30 +27,75 @@ let index = 0;
 }());
 
 
-new controllers.Slider('radius', 0, 10, 1, 0, '', 'default', '#controllers', (value) => {
-  bpf.params.set('radius', value);
+new controllers.Slider({
+  label: 'radius',
+  min: 0,
+  max: 10,
+  step: 1,
+  default: 0,
+  size: 'default',
+  container: '#controllers',
+  callback: (value) => bpf.params.set('radius', value),
 });
 
-new controllers.Toggle('line', true, '#controllers', (value) => {
-  bpf.params.set('line', value);
+new controllers.Toggle({
+  label: 'line',
+  active: true,
+  container: '#controllers',
+  callback: (value) => bpf.params.set('line', value),
 });
 
-new controllers.Slider('min', -10, 0, 0.01, -1, '', 'default', '#controllers', (value) => {
-  bpf.params.set('min', value);
+new controllers.Slider({
+  label: 'min',
+  min: -10,
+  max: 0,
+  step: 0.01,
+  default: -1,
+  size: 'default',
+  container: '#controllers',
+  callback: (value) => bpf.params.set('min', value),
 });
 
-new controllers.Slider('max', 0, 10, 0.01, 1, '', 'default', '#controllers', (value) => {
-  bpf.params.set('max', value);
+new controllers.Slider({
+  label: 'max',
+  min: 0,
+  max: 10,
+  step: 0.01,
+  default: 1,
+  size: 'default',
+  container: '#controllers',
+  callback: (value) => bpf.params.set('max', value),
 });
 
-new controllers.Slider('duration', 1, 20, 0.1, 3, '', 'default', '#controllers', (value) => {
-  bpf.params.set('duration', value);
+new controllers.Slider({
+  label: 'duration',
+  min: 1,
+  max: 20,
+  step: 0.1,
+  default: 3,
+  size: 'default',
+  container: '#controllers',
+  callback: (value) => bpf.params.set('duration', value),
 });
 
-new controllers.Slider('width', 300, 400, 1, 300, '', 'default', '#controllers', (value) => {
-  bpf.params.set('width', value);
+new controllers.Slider({
+  label: 'width',
+  min: 300,
+  max: 400,
+  step: 1,
+  default: 300,
+  size: 'default',
+  container: '#controllers',
+  callback: (value) => bpf.params.set('width', value),
 });
 
-new controllers.Slider('height', 150, 200, 1, 150, '', 'default', '#controllers', (value) => {
-  bpf.params.set('height', value);
+new controllers.Slider({
+  label: 'height',
+  min: 150,
+  max: 200,
+  step: 1,
+  default: 150,
+  size: 'default',
+  container: '#controllers',
+  callback: (value) => bpf.params.set('height', value),
 });
