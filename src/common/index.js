ca71e486e763b38bfd9e8b7c893b3fdafb5e293d
Benjamin Matuszewski <benjamin.matuszewski@gmail.com>
2017-03-31 11:33:23 +0200

    exposed core namespace

--- src/common/index.js
+++ src/common/index.js
@@ -1,5 +1,8 @@
 export const version = '%version%';
 
+import * as _core from '../core';
+export const core = _core;
+
 export { default as operator } from './operator/_namespace';
 export { default as sink } from './sink/_namespace';
 export { default as source } from './source/_namespace';
